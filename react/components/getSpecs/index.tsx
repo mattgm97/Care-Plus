d9b7be067cbb8f043314c58ad1bc83cfdb2b30fd
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2023-01-07 23:00:15 -0300

    more home

--- react/components/getSpecs/index.tsx
+++ react/components/getSpecs/index.tsx
@@ -2,19 +2,61 @@ import React from "react";
 import { useProduct } from "vtex.product-context";
 import "./global.css";
 interface ProductAvailableProps {
-    // children: any;
-};
+  // children: any;
+}
 
 const getSpecs: StorefrontFunctionComponent<ProductAvailableProps> = () => {
-    const productInfo = useProduct();
-    const specs = productInfo?.product?.properties[0].values;
-    console.log(specs)
-//verify if bigger than 7 to create 2 sides
- 
-        return (
-         <>
-         </>
-        );
-    };
+  const productInfo = useProduct();
+  const specs = productInfo?.product?.properties[0].values;
+  console.log(productInfo);
+  let specsGrid1 = [];
+  let specsGrid2 = [];
+
+  for (let i = 0; i < specs!.length; i++) {
+    console.log(specs![i]);
+    let okIcon = "";
+    if (productInfo?.product?.properties[0].name == "LITE") {
+      okIcon = "ok-roxo.svg";
+    } else if (productInfo?.product?.properties[0].name == "PRO") {
+      okIcon = "ok-verde.svg";
+    }
+
+    switch (specs![i]) {
+      case "Clareamento":
+      case "Ortodontia":
+      case "Prótese":
+      case "Alinhador":
+        okIcon = "ok-azul.svg";
+        break;
+      case "Acesso a Clínicas próprias":
+        okIcon = "ok-azul_marinho.svg";
+        break;
+    }
+    if (i < 7) {
+      specsGrid1.push(
+        <li key={specs![i]}>
+          <img src={`/arquivos/${okIcon}`} />
+          <p>{specs![i]}</p>
+        </li>
+      );
+    } else {
+      specsGrid2.push(
+        <li key={specs![i]}>
+          <img src={`/arquivos/${okIcon}`} />
+          <p>{specs![i]}</p>
+        </li>
+      );
+    }
+  }
+
+  return (
+    <>
+      <div className="product-specifications">
+        <ul className="col">{specsGrid1}</ul>
+        <ul className="col">{specsGrid2}</ul>
+      </div>
+    </>
+  );
+};
 
 export default getSpecs;
